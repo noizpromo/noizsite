7c95e4acf50103c964f6b770be88479f253a0894
Phil Wheldon <philip.wheldon@googlemail.com>
2017-01-04 21:59:22 +0000

    Config settings

--- server/config.py
+++ server/config.py
@@ -4,9 +4,9 @@ SMTP_ADDRESS = "smtp.mailgun.org"
 SMTP_PORT = 587
 
 EMAIL_FROM_ADDRESS = "web@noizpromotions.com"
-EMAIL_BOOKING_ADDRESS = "philip.wheldon@googlemail.com"
-EMAIL_MAILINGLIST_ADDRESS = "philip.wheldon@googlemail.com"
-EMAIL_INFO_ADDRESS = "philip.wheldon@googlemail.com"
+EMAIL_BOOKING_ADDRESS = "bookings@noizpromotions.com"
+EMAIL_MAILINGLIST_ADDRESS = "mailinglist@noizpromotions.com"
+EMAIL_INFO_ADDRESS = "info@noizpromotions.com"
 
 DB_NAME = "noiz"
 DB_USERNAME = "noiz_site"
